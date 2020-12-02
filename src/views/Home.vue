809f16b5c8bfebe0d873e5dea3796410ce322bc2
Sahal Zain <zean00zone@gmail.com>
2020-12-02 14:35:14 +0700

    using custom endpoints

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,17 @@
         <v-row>
             <v-col>
                 <file-browser
-                    
+                    :axiosConfig="{baseURL: '__AXIOS_BASE_URL__'}"
                     :maxUploadFileSize="5242880"
                     :maxUploadFilesCount="5"
                     :storages="'local'"
+                    :endpoints="{
+                        list: { url: '/folder{path}', method: 'get' },
+                        upload: { url: '/files{path}', method: 'post' },
+                        mkdir: { url: '/files{path}', method: 'put' },
+                        delete: { url: '/files{path}', method: 'delete' },
+                        download : { url: '/files{path}', method: 'get' },
+                    }"
                 />
             </v-col>
         </v-row>
